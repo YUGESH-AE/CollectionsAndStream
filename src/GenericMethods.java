8baca3cd4c7cdd51ecec2d317d45c70c1f4dffc8
A.E, Yugesh <yugesh.elumalai@agilysys.com>
2025-02-23 17:20:57 +0530

    GenericMethods

--- src/GenericMethods.java
+++ src/GenericMethods.java
@@ -30,4 +30,11 @@ public class GenericMethods {
         }
         return count;
     }
+
+    public <T extends Comparable<T>> T minValue(T t1,T t2){
+        if(t1.compareTo(t2)<0){
+            return t1;
+        }
+        return t2;
+    }
 }
